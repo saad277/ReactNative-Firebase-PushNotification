3c259a760a52eda1aaf400e370419ea11eb09fcf
saad277 <saadsheikh27@gmail.com>
2020-03-21 22:43:00 +0500

    notification working

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ import {
   View,
   Text,
   StatusBar,
+  AsyncStorage,
 } from 'react-native';
 
 import firebase from 'react-native-firebase'
@@ -15,54 +16,81 @@ import firebase from 'react-native-firebase'
 
 class App extends Component {
 
+  async getToken() {
 
-  state={
+    let fcmToken = await AsyncStorage.getItem("fcmToken");
 
+    if (!fcmToken) {
+
+      fcmToken = await firebase.messaging().getToken();
+
+      if (fcmToken) {
+
+        await AsyncStorage.setItem("fcmToken", fcmToken)
+      }
+
+
+
+    }
+
+  }
+
+
+  async requestPermission() {
+
+    try {
+      await firebase.messaging().requestPermission();
+      this.getToken();
+
+    } catch (error) {
+
+      console.log("Permission Rejected")
+    }
+  }
+
+
+  async checkPermission() {
+
+    const enabled = await firebase.messaging().hasPermission();
+
+
+    if (enabled) {
+
+      this.getToken();
+
+    } else {
+
+      this.requestPermission();
+    }
+
+
+  }
+
+
+  async createNotificationListener() {
+
+    firebase.notifications().onNotification((notification) => {
+
+
+      notification.android.setChannelId("insider").setSound("default");
+      firebase.notifications().displayNotification(notification)
+    })
   }
 
   componentDidMount() {
 
-    const getFood = async () => {
-
-      var foodList = []
-
-      var snapshot = await firebase.firestore().collection("food").get();
-
-
-
-      snapshot.forEach((doc) => {
-
-        const foodItem = doc.data();
-
-       
-
-        foodItem.id = doc.id;                     // get id 
-
-        foodList.push(foodItem);
-
-      })
-      this.setState({
-
-        foods: [...foodList]
-
-      })
-
-    }
-
-    getFood();
-
-    
-
-
+    const channel = new firebase.notifications.Android.Channel("insider", "insider channel", firebase.notifications.Android.Importance.Max);
+    firebase.notifications().android.createChannel(channel);
+    this.checkPermission();
+    this.createNotificationListener();
 
   }
-
 
   render() {
 
     return (
-      <View>
-        <Text>firebase</Text>
+      <View style={styles.container}>
+        <Text style={styles.text}>Push Notifications</Text>
       </View>
     )
 
@@ -72,6 +100,26 @@ class App extends Component {
 
 
 }
+
+
+const styles = StyleSheet.create({
+
+  container: {
+
+    alignItems: "center",
+    justifyContent: "center"
+
+  },
+
+  text: {
+
+    fontSize: 28,
+
+
+  }
+
+
+})
 
 
 export default App;
--- android/app/src/main/java/com/pushnoti/MainApplication.java
+++ android/app/src/main/java/com/pushnoti/MainApplication.java
@@ -15,33 +15,40 @@ import io.invertase.firebase.RNFirebasePackage;
 import io.invertase.firebase.firestore.RNFirebaseFirestorePackage;
 import io.invertase.firebase.storage.RNFirebaseStoragePackage;
 import io.invertase.firebase.auth.RNFirebaseAuthPackage;
+import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
+import io.invertase.firebase.notifications.RNFirebaseNotificationsPackage;
+
+
+
 
 public class MainApplication extends Application implements ReactApplication {
 
-  private final ReactNativeHost mReactNativeHost =
-      new ReactNativeHost(this) {
-        @Override
-        public boolean getUseDeveloperSupport() {
-          return BuildConfig.DEBUG;
-        }
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    @Override
+    public boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
 
-        @Override
-        protected List<ReactPackage> getPackages() {
-          @SuppressWarnings("UnnecessaryLocalVariable")
-          List<ReactPackage> packages = new PackageList(this).getPackages();
-          // Packages that cannot be autolinked yet can be added manually here, for example:
-          // packages.add(new MyReactNativePackage());
-          packages.add(new RNFirebaseAuthPackage());
-        packages.add(new RNFirebaseFirestorePackage());
-        packages.add(new RNFirebaseStoragePackage());
-          return packages;
-        }
+    @Override
+    protected List<ReactPackage> getPackages() {
+      @SuppressWarnings("UnnecessaryLocalVariable")
+      List<ReactPackage> packages = new PackageList(this).getPackages();
+      // Packages that cannot be autolinked yet can be added manually here, for
+      // example:
+      // packages.add(new MyReactNativePackage());
+      packages.add(new RNFirebaseAuthPackage());
+      packages.add(new RNFirebaseFirestorePackage());
+      packages.add(new RNFirebaseStoragePackage());
+      packages.add(new RNFirebaseMessagingPackage());
+      packages.add(new RNFirebaseNotificationsPackage());
+      return packages;
+    }
 
-        @Override
-        protected String getJSMainModuleName() {
-          return "index";
-        }
-      };
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
+    }
+  };
 
   @Override
   public ReactNativeHost getReactNativeHost() {
@@ -64,9 +71,9 @@ public class MainApplication extends Application implements ReactApplication {
     if (BuildConfig.DEBUG) {
       try {
         /*
-         We use reflection here to pick up the class that initializes Flipper,
-        since Flipper library is not available in release mode
-        */
+         * We use reflection here to pick up the class that initializes Flipper, since
+         * Flipper library is not available in release mode
+         */
         Class<?> aClass = Class.forName("com.facebook.flipper.ReactNativeFlipper");
         aClass.getMethod("initializeFlipper", Context.class).invoke(null, context);
       } catch (ClassNotFoundException e) {
